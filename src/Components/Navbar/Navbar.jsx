934fc636cd299fd1d43bb35c1d62c77c96804551
creamyngreen <vipboy20031408@gmail.com>
2024-10-11 10:35:00 +0700

    style: update landing page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,20 +4,20 @@ import Logo from '../../assets/Logo/Logo.png';
 const Navbar = () => {
 return (
 <nav className="bg-white py-4">
-    <div className="container flex justify-between items-center">
+    <div className="flex justify-between items-center">
 
     <div className="flex items-center ml-10">
         <img src={Logo} alt="EffiRoute Logo" className="h-10 w-auto mr-2 mb-5" /> 
-        <span className="text-[#56C074] font-bold text-2xl font-sora">EffiRoute</span>
+        <span className="text-[#56C074] font-bold text-3xl font-sora">EffiRoute</span>
     </div>
 
-    <div className="flex space-x-20 ml-auto"> 
-        <a href="/solution" className="text-gray-800 hover:text-[#3596FF] font-noto">Solution</a>
-        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto">Services</a>
-        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto">Why choose us</a>
-        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto">News</a>
-        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto">Pricing</a>
-        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto">Sign In</a>
+    <div className="flex justify-items-end space-x-20 mr-12"> 
+        <a href="/solution" className="text-gray-800 hover:text-[#3596FF] font-noto text-xl">Solution</a>
+        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto text-xl">Services</a>
+        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto text-xl">Why choose us</a>
+        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto text-xl">News</a>
+        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto text-xl">Pricing</a>
+        <a href="#" className="text-gray-800 hover:text-[#3596FF] font-noto text-xl">Sign In</a>
     </div>
     </div>
 </nav>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -6,28 +6,27 @@ import PlanExample from "../../assets/Home/PlanExample.png";
 const Home = () => {
     return (
         <div>
-            <div className="flex justify-between items-center p-10">
-                <div className="flex flex-col space-y-4 ml-96">
-                    <h1 className="text-6xl font-bold font-poppins">
-                        Optimize Your <br />
-                        Delivery Routes<br />
+            <div className="flex justify-between p-10">
+                <div className="flex flex-col space-y-4 ml-28">
+                    <h1 className="text-8xl font-bold font-poppins">
+                        Optimize Your Delivery Routes<br />
                         with AI Precision
                     </h1>
-                    <p className="text-lg font-quattrocento">
+                    <p className="text-xl font-quattrocento">
                         Save up to 30% on fuel costs and<br /> 
                         reduce delivery times with our<br /> 
                         all-in-one route optimization platform.
                     </p>
                     <button
                         style={{ width: '150px' }} 
-                        className="mt-4 bg-[#FD7E14] text-white py-2 px-4 rounded font-bold font-quattrocento"
+                        className="mt-4 bg-[#FD7E14] text-white text-lg py-2 px-4 rounded font-bold font-quattrocento"
                     >
                         Start Free Trial
                     </button>
                 </div>
 
                 <div>
-                    <img src={HomePage} alt="Delivery Optimization" className="w-9/12 h-auto mr-96 mt-28" />
+                    <img src={HomePage} alt="Delivery Optimization" className="w-9/12 h-auto mr-96" />
                 </div>
             </div>
 
@@ -45,7 +44,7 @@ const Home = () => {
                     >
                         1B+
                     </span>
-                        <span className="font-lato mt-2 font-semibold">Miles Optimized</span>
+                        <span className="font-lato mt-2 font-semibold text-lg ">Miles Optimized</span>
                     </div>
 
                     <div className="flex flex-col items-center">
@@ -58,7 +57,7 @@ const Home = () => {
                     >
                         200M+
                     </span>
-                        <span className="font-lato mt-2 font-semibold">Stop Visited</span>
+                        <span className="font-lato mt-2 font-semibold text-lg">Stop Visited</span>
                     </div>
 
                     <div className="flex flex-col items-center">
@@ -71,7 +70,7 @@ const Home = () => {
                     >
                         30M+
                     </span>
-                        <span className="font-lato mt-2 font-semibold">Route Planned</span>
+                        <span className="font-lato mt-2 font-semibold text-lg">Route Planned</span>
                     </div>
 
                     <div className="flex flex-col items-center">
@@ -84,7 +83,7 @@ const Home = () => {
                     >
                         50K+
                     </span>
-                        <span className="font-lato mt-2 font-semibold">Happy Customers</span>
+                        <span className="font-lato mt-2 font-semibold text-lg">Happy Customers</span>
                     </div>
                 </div>
             </div>
